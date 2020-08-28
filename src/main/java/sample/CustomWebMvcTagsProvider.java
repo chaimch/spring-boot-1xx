db253dc8558180ed0b54cde7231f948d256a6b61
Chaim <chenming@dayuwuxian.com>
2020-08-28 11:20:14 +0800

    spring-boot 1xx 自定义 metrics

---
+++ src/main/java/sample/CustomWebMvcTagsProvider.java
@@ -0,0 +1,47 @@
+package sample;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.lang.NonNullApi;
+import io.micrometer.core.lang.Nullable;
+import io.micrometer.spring.web.servlet.DefaultWebMvcTagsProvider;
+import io.micrometer.spring.web.servlet.WebMvcTags;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+
+@Configuration
+@ConditionalOnClass(DefaultWebMvcTagsProvider.class)
+@NonNullApi
+public class CustomWebMvcTagsProvider extends DefaultWebMvcTagsProvider {
+    @Override
+    public Iterable<Tag> httpLongRequestTags(@Nullable HttpServletRequest request, @Nullable Object handler) {
+        String degradeLevelKey = "degrade_level";
+        return Arrays.asList(WebMvcTags.method(request),
+                WebMvcTags.uri(request, null),
+                request == null || request.getHeader(degradeLevelKey) == null ?
+                        Tag.of(degradeLevelKey, "0") :
+                        Tag.of("degrade_level", request.getHeader(degradeLevelKey))
+        );
+    }
+
+    @Override
+    public Iterable<Tag> httpRequestTags(@Nullable HttpServletRequest request,
+                                         @Nullable HttpServletResponse response,
+                                         @Nullable Object handler,
+                                         @Nullable Throwable ex) {
+        String degradeLevelKey = "degrade_level";
+        return Arrays.asList(
+                WebMvcTags.method(request),
+                WebMvcTags.uri(request, response),
+                WebMvcTags.exception(ex),
+                WebMvcTags.status(response),
+                request == null || request.getHeader(degradeLevelKey) == null ?
+                        Tag.of(degradeLevelKey, "0") :
+                        Tag.of("degrade_level", request.getHeader(degradeLevelKey))
+                );
+    }
+}
---
+++ src/main/java/sample/HealthController.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Health ok() {
+        return Health.status("ok").build();
+    }
+}
+
